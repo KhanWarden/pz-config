27a576a6eb138c1cff6e6691a4f8356b23f1c5cc
mr_irmag <mrirmag.pr@gmail.com>
2026-06-13 14:34:36 +0500

    feat(map): add new spawnpoints

--- servertest_spawnregions.lua
+++ servertest_spawnregions.lua
@@ -1,10 +1,14 @@
 function SpawnRegions()
 	return {
-		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
-		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
-		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
-		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
-		-- Uncomment the line below to add a custom spawnpoint for this server.
---		{ name = "Twiggy's Bar", serverfile = "servertest_spawnpoints.lua" },
-	}
+        -- Добавленные города из мода
+        { name = "Brandenburg, KY", file = "media/maps/Brandenburg, KY/spawnpoints.lua" },
+        { name = "Ekron, KY", file = "media/maps/Ekron, KY/spawnpoints.lua" },
+        { name = "Irvington, KY", file = "media/maps/Irvington, KY/spawnpoints.lua" },
+        
+        -- Стандартные города
+        { name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+        { name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+        { name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+        { name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+    }
 end
